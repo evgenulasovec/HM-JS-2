c095e2cec2684d51237465b3652cdd6b95d07805
evgenulasovec <133598998+evgenulasovec@users.noreply.github.com>
2023-06-19 16:49:34 +0300

    Add files via upload

---
+++ JS-2/scripts.js
@@ -0,0 +1,28 @@
+//вывести в консоль n 10 раз
+
+// for( let i = 0; i < 10; i++){
+//     console.log ('n');
+// }
+
+
+// for( let i = 0; i <= 10; i++) {
+// console.log (i);
+// }
+
+// функции 
+
+// function isMultiple (num1, num2) {
+// const result = num1 % num2 ===0;
+// return result;
+// то же самое
+// return num1 % num2 ===0;
+// }
+
+// isMultiple (9, 3);
+// isMultiple (22, 2);
+
+// height:200px;
+const addHeight = (num) => {
+
+
+}
